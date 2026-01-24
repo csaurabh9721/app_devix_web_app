e6bacc2603470c645549e83ac93179258313e128
Saurabh Chauhan <saurabh.chauhan@jilit.co.in>
2026-01-24 12:28:18 +0530

    feat: implement contact form submission API
    - **Add `http` Dependency:**
      - Add the `http` package to `pubspec.yaml` to enable API calls.
    
    - **Implement Form Submission Logic:**
      - Convert `_ContactForm` from a `StatelessWidget` to a `StatefulWidget` to manage form state and API calls.
      - Implement form validation, particularly for the email field.
      - Add `_isLoading` `ValueNotifier` to show a `CircularProgressIndicator` during submission.
      - Create a `_callApi` method to send form data (`name`, `email`, `phone`, `subject`, `message`) to the `https://app-devix-backend.onrender.com/api/contact/request` endpoint via a POST request.
    
    - **Enhance User Feedback:**
      - Display a success `SnackBar` on a successful (200) response.
      - Show an error `SnackBar` if the API call fails or encounters an exception.
    
    - **Refactor Form Fields:**
      - Replace `TextField` with `TextFormField` to integrate with the `Form` widget for validation.
      - Add `TextEditingController`s to all fields to manage input values.
      - Add a new "Your Mobile" field to the form.

--- lib/view/contact_section.dart
+++ lib/view/contact_section.dart
@@ -1,4 +1,9 @@
+import 'dart:convert';
+
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
+
+import 'package:http/http.dart' as http;
 
 class ContactSection extends StatelessWidget {
   const ContactSection({super.key});
@@ -20,26 +25,27 @@ class ContactSection extends StatelessWidget {
           constraints: const BoxConstraints(maxWidth: 1200),
           child: isMobile
               ? Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: const [
-              _ContactIntro(),
-              SizedBox(height: 40),
-              _ContactForm(),
-            ],
-          )
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: const [
+                    _ContactIntro(),
+                    SizedBox(height: 40),
+                    _ContactForm(),
+                  ],
+                )
               : Row(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: const [
-              Expanded(flex: 5, child: _ContactIntro()),
-              SizedBox(width: 60),
-              Expanded(flex: 6, child: _ContactForm()),
-            ],
-          ),
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: const [
+                    Expanded(flex: 5, child: _ContactIntro()),
+                    SizedBox(width: 60),
+                    Expanded(flex: 6, child: _ContactForm()),
+                  ],
+                ),
         ),
       ),
     );
   }
 }
+
 class _ContactIntro extends StatelessWidget {
   const _ContactIntro();
 
@@ -69,90 +75,182 @@ class _ContactIntro extends StatelessWidget {
         SizedBox(height: 18),
         Text(
           'Have an idea, a project, or just want to talk tech?\n'
-              'We’d love to hear from you. Tell us about your goals and we’ll help you shape the solution.',
-          style: TextStyle(
-            fontSize: 16.5,
-            height: 1.8,
-            color: Colors.black54,
-          ),
+          'We’d love to hear from you. Tell us about your goals and we’ll help you shape the solution.',
+          style: TextStyle(fontSize: 16.5, height: 1.8, color: Colors.black54),
         ),
         SizedBox(height: 28),
         Text(
           '✔ Fast response\n'
-              '✔ Clear communication\n'
-              '✔ Scalable solutions',
-          style: TextStyle(
-            fontSize: 15.5,
-            height: 1.8,
-            color: Colors.black87,
-          ),
+          '✔ Clear communication\n'
+          '✔ Scalable solutions',
+          style: TextStyle(fontSize: 15.5, height: 1.8, color: Colors.black87),
         ),
       ],
     );
   }
 }
-class _ContactForm extends StatelessWidget {
+
+class _ContactForm extends StatefulWidget {
   const _ContactForm();
 
   @override
-  Widget build(BuildContext context) {
-    return Column(
-      crossAxisAlignment: CrossAxisAlignment.start,
-      children: [
-        _Field(label: 'Your Name'),
-        const SizedBox(height: 18),
-        _Field(label: 'Email Address'),
-        const SizedBox(height: 18),
-        _Field(label: 'Project Type (Web / Mobile / Other)'),
-        const SizedBox(height: 18),
-        _Field(
-          label: 'Tell us about your project',
-          maxLines: 5,
-        ),
-        const SizedBox(height: 30),
+  State<_ContactForm> createState() => _ContactFormState();
+}
 
-        /// CTA
-        SizedBox(
-          width: double.infinity,
-          child: ElevatedButton(
-            onPressed: () {
-              //https://app-devix-backend.onrender.com/api/contact/request
-            //   {
-            //     "name": "Test User1",
-            //   "email": "csaurabh002@gmail.com",
-            //   "phone": "9455122458",
-            //   "subject": "Live Testing",
-            //   "message": "Test by live"
-            // }
+class _ContactFormState extends State<_ContactForm> {
+  final GlobalKey<FormState> _formKey = GlobalKey<FormState>();
+  final TextEditingController _nameController = TextEditingController();
+  final TextEditingController _emailController = TextEditingController();
+  final TextEditingController _phoneController = TextEditingController();
+  final TextEditingController _subjectController = TextEditingController();
+  final TextEditingController _messageController = TextEditingController();
+  final ValueNotifier<bool> _isLoading =  ValueNotifier<bool>(false);
+
+  @override
+  Widget build(BuildContext context) {
+    return Form(
+      key: _formKey,
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          _Field(label: 'Your Name', controller: _nameController),
+          const SizedBox(height: 18),
+          _Field(label: 'Your Mobile', controller: _phoneController),
+          const SizedBox(height: 18),
+          _Field(
+            label: 'Email Address',
+            controller: _emailController,
+            validator: (v) {
+              if (v == null || v.isEmpty) {
+                return 'This field is required';
+              }
+              if (!v.contains('@')) {
+                return 'Invalid email address';
+              }
+              return null;
             },
-            style: ElevatedButton.styleFrom(
-              backgroundColor: Colors.indigo,
-              padding: const EdgeInsets.symmetric(vertical: 16),
-              shape: RoundedRectangleBorder(
-                borderRadius: BorderRadius.circular(8),
-              ),
-            ),
-            child: const Text(
-              'Send Message',
-              style: TextStyle(
-                fontSize: 16,
-                fontWeight: FontWeight.w600,
-                color: Colors.white,
-              ),
-            ),
           ),
-        ),
-      ],
+          const SizedBox(height: 18),
+          _Field(
+            label: 'Project Type (Web / Mobile / Other)',
+            controller: _subjectController,
+          ),
+          const SizedBox(height: 18),
+          _Field(
+            label: 'Tell us about your project',
+            maxLines: 5,
+            controller: _messageController,
+          ),
+          const SizedBox(height: 30),
+
+          /// CTA
+          ValueListenableBuilder(
+            valueListenable: _isLoading,
+            builder: (context, value, child) {
+              if (value) {
+                return const Center(
+                  child: CircularProgressIndicator(),
+                );
+              }
+              return SizedBox(
+                width: double.infinity,
+                child: ElevatedButton(
+                  onPressed: _onSave,
+                  style: ElevatedButton.styleFrom(
+                    backgroundColor: Colors.indigo,
+                    padding: const EdgeInsets.symmetric(vertical: 16),
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(8),
+                    ),
+                  ),
+                  child: const Text(
+                    'Send Message',
+                    style: TextStyle(
+                      fontSize: 16,
+                      fontWeight: FontWeight.w600,
+                      color: Colors.white,
+                    ),
+                  ),
+                ),
+              );
+            }
+          ),
+        ],
+      ),
     );
   }
+
+  void _onSave() {
+    if (_formKey.currentState!.validate()) {
+      _callApi();
+    }
+  }
+
+  Future<void> _callApi() async {
+    try {
+      _isLoading.value = true;
+      Uri url = Uri.parse(
+        'https://app-devix-backend.onrender.com/api/contact/request',
+      );
+      Map<String, String> headers = {'Content-Type': 'application/json'};
+      Map<String, dynamic> body = {
+        "name": _nameController.text,
+        "email": _emailController.text,
+        "phone": _phoneController.text,
+        "subject": _subjectController.text,
+        "message": _messageController.text,
+      };
+      if (kDebugMode) {
+        print(body);
+      }
+
+      http.Response response = await http.post(
+        url,
+        headers: headers,
+        body: json.encode(body),
+      );
+      if (kDebugMode) {
+        print(response.body);
+      }
+      if (response.statusCode == 200) {
+        ScaffoldMessenger.of(context).showSnackBar(
+          SnackBar(
+            backgroundColor: Color(0xFF2E7D32),
+            content: Text(
+              'Message sent successfully! We will get back to you soon.',
+            ),
+          ),
+        );
+      } else {
+        throw Exception('Failed to send form');
+      }
+    } catch (e) {
+      if (kDebugMode) {
+        print(e);
+      }
+      ScaffoldMessenger.of(context).showSnackBar(
+        SnackBar(
+          backgroundColor: Color(0xFFC62828),
+          content: Text('Failed to send message. Please try again later.'),
+        ),
+      );
+    }finally {
+      _isLoading.value = false;
+    }
+  }
 }
+
 class _Field extends StatelessWidget {
   final String label;
   final int maxLines;
+  final TextEditingController controller;
+  final String? Function(String?)? validator;
 
   const _Field({
     required this.label,
     this.maxLines = 1,
+    required this.controller,
+    this.validator,
   });
 
   @override
@@ -169,14 +267,25 @@ class _Field extends StatelessWidget {
           ),
         ),
         const SizedBox(height: 6),
-        TextField(
+        TextFormField(
+          validator:
+              label == 'Tell us about your project' || label == 'Your Mobile'
+              ? null
+              : validator ??
+                    (v) {
+                      if (v == null || v.isEmpty) {
+                        return 'This field is required';
+                      }
+                      return null;
+                    },
+          controller: controller,
           maxLines: maxLines,
           decoration: InputDecoration(
-            contentPadding:
-            const EdgeInsets.symmetric(horizontal: 14, vertical: 14),
-            border: OutlineInputBorder(
-              borderRadius: BorderRadius.circular(8),
+            contentPadding: const EdgeInsets.symmetric(
+              horizontal: 14,
+              vertical: 14,
             ),
+            border: OutlineInputBorder(borderRadius: BorderRadius.circular(8)),
             focusedBorder: OutlineInputBorder(
               borderRadius: BorderRadius.circular(8),
               borderSide: const BorderSide(color: Colors.indigo),
